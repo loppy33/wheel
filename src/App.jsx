d0be582e8b380c45deb54eb23c7f610f0456f0e3
loppy33 <nikita.petrenko1406@gmail.com>
2024-05-03 13:42:44 +0200

    m

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,62 @@
-import React from 'react';
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import React, { useState, useEffect } from 'react';
 import './App.sass';
-import WheelPage from './components/wheelPage';
 
+import Wheel from './components/wheel/wheel';
+import Modal from './components/modal/modal';
+import PassportScanner from './components/passScaner/PassportScanner';
 
 function App() {
+  const [result, setResult] = useState(null);
+  const [passScaner, setPassScaner] = useState();
+
+  const [front, setFront] = useState();
+  const [back, setBack] = useState();
+  const [selfie, setSelfie] = useState();
+
+  useEffect(() => {
+    // Добавляем пиксель Facebook при монтировании компонента
+    const script = document.createElement('script');
+    script.innerHTML = `
+      !function(f,b,e,v,n,t,s)
+      {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+      n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+      if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+      n.queue=[];t=b.createElement(e);t.async=!0;
+      t.src=v;s=b.getElementsByTagName(e)[0];
+      s.parentNode.insertBefore(t,s)}(window, document,'script',
+      'https://connect.facebook.net/en_US/fbevents.js');
+      fbq('init', '770558885176026');
+      fbq('track', 'PageView');
+    `;
+    document.head.appendChild(script);
+
+    return () => {
+      // Удаление пикселя Facebook при размонтировании компонента
+      document.head.removeChild(script);
+    };
+  }, []);
 
   return (
-    <BrowserRouter>
-      <Routes>
-      <Route index element={<WheelPage />} />
-        <Route exact path="/" component={<WheelPage />} />
-        <Route path="/wheel" component={<WheelPage /> } />
-      </Routes>
-    </BrowserRouter>
+    <div className="App">
+      <div className="container">
+        {passScaner ? (
+          <PassportScanner
+            front={front}
+            setFront={setFront} setBack={setBack} setSelfie={setSelfie}
+            passScaner={passScaner} setPassScaner={setPassScaner}
+          ></PassportScanner>
+        ) : (
+          <>
+            <Wheel setResult={setResult}></Wheel>
+            <Modal
+              setFront={setFront} setBack={setBack} setSelfie={setSelfie}
+              front={front} back={back} selfie={selfie}
+              setPassScaner={setPassScaner} result={result}
+            ></Modal>
+          </>
+        )}
+      </div>
+    </div>
   );
 }
 
